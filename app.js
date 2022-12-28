b6be64bed2e8b22d94e5ce461d083a9b1383607a
electronico15 <74164067+electronico15@users.noreply.github.com>
2022-12-28 15:32:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { Console } = require('console');
 var command = ffmpeg();
 command.setFfmpegPath(ejec)
 
-app.use(express.static(__dirname + '/dow'));
+//app.use(express.static(__dirname + '/dow'));
 
 app.use(body_parser.urlencoded({extended:true}));
 
