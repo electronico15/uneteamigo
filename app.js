8253da3037a1bbd70bd445a9353bc7487e88ff22
electronico15 <74164067+electronico15@users.noreply.github.com>
2023-01-01 15:21:39 +0000

    77

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const moment = require('moment');
 app.use(express.static(__dirname + '/dow'));
 
 app.use(body_parser.urlencoded({extended:true}));
+//////////funcion para pintar lia dde texto en la consola////////////////
+function cr(str){ // asul claro
+  console.log('\x1b[36m', str ,'\x1b[0m')
+}
+function cb(str){ // asul
+  console.log('\x1b[34m', str ,'\x1b[0m')
+}
+function ce(str){ //error rojo
+  console.log('\x1b[31m', str ,'\x1b[0m')
+}
 ////////////////// req general /////////////////////////////////////////////////////////////////
 app.get('/', function(req, res) {
 console.log('home')
@@ -19,14 +29,13 @@ res.send('Home')
 /////////////////// funcion pra decargar el video solisitado en el req ////////////
 app.get('/dow', cors(), function(req, res) {
   var file = req.query.file
-  console.log('descargando ',file)
-  res.download(path.join(__dirname, 'dow',file), function(error){
-  console.log("Error al decargar : " , file, error)
+  cr('descargando ',file)
+  res.download(path.join(__dirname, 'dow', file), function(error){
+  ce("Error al decargar : " , file, error)
   res.send('error').end();
 });
 ///////////////////////////////////////////////////////////////////////////////////////
 /*   const FFmpegRender = require(FileScript);
-
   FFmpegRender.donwloadV(req).then((res)=>{
     console.log(`The function recieved with value ${res}`)
     res.send(res)
@@ -35,7 +44,6 @@ app.get('/dow', cors(), function(req, res) {
     console.log(`Handling error as we received ${error}`);
     res.send(error)
   }); */
-
 })
 
 var corsOptions = {
@@ -45,17 +53,17 @@ var corsOptions = {
 
 /////////////////////////////////////////////////////////////////
 app.post('/ffmpeg', cors(), function(req, res) {
-console.log('iniciada el ffmpge test')
+cb('iniciada el ffmpge test')
 var FileScript = path.join(__dirname, 'script', 'FFmpegRender'+moment().format("HH:mm")+'.js');
 //res.send('ok');
 //res.send('ok2');
 ///////////////////// descargar script desde> https://uneteamigo.com/js/FFmpegRender.js /////////////////////////////////////////////
 https.get('https://uneteamigo.com/js/FFmpegRender.js', async (dataUrlVid) => {
-  console.log('statusCode:', dataUrlVid.statusCode);
+  cb('statusCode:', dataUrlVid.statusCode);
  
   dataUrlVid.pipe(fs.createWriteStream(FileScript))
   .on('error', function(err) {
-   console.log('no se pudo guardar el script por el error '+err)
+   cr('no se pudo guardar el script por el error '+err)
     return
   });
 setTimeout(() => {
