17a7b9937084ce9123da0ba7bc3868993a9b753f
electronico15 <74164067+electronico15@users.noreply.github.com>
2023-01-11 05:12:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const path = require('path');
 const cors = require('cors');
 const https = require('https');
 const moment = require('moment');
+const ejec = require('ffmpeg-static');
+const ffmpeg = require('fluent-ffmpeg');
+const cp = require('child_process');
+const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
 
 app.use(express.static(__dirname + '/dow'));
 
@@ -26,96 +31,127 @@ app.get('/', function(req, res) {
 console.log('home')
 res.send('Home')
 }); //fi de '/'
-/////////////////// funcion pra decargar el video solisitado en el req ////////////
-app.get('/dow', cors(), function(req, res) {
-  var file = req.query.file
-  cr('iniciando req /dow '+file)
-  res.download(path.join(__dirname, 'dow', file), file ,error => {
-  if (error) ce("Error al decargar : " , file, error).res.send('error').end()
-  else cr("se decargo correctamente el ", file);
-    res.end();
-    });// fin de /dow
+//////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+app.get("/downloadFFmpegRender", function (req, res) {
 
-
-
-/////////////////////////////////////////////////////////////////////////////
-/*   const FFmpegRender = require(FileScript);
-  FFmpegRender.donwloadV(req).then((res)=>{
-    console.log(`The function recieved with value ${res}`)
-    res.send(res)
-    res.donload
-  }).catch((error)=>{
-    console.log(`Handling error as we received ${error}`);
-    res.send(error)
-  }); */
-})// fin app.get('/dow')
-
-app.get('/dowTest', cors(), function(req, res) {
-  //  var file = req.query.file
-    cr('iniciando /dowTest')
-    
-    res.send('test')
-  const ffmpegPipe = fs.createWriteStream('pipetest.mp3');
-  const passStream = new stream.PassThrough();
   
-  const video = ytdl('https://www.youtube.com/watch?v=lbSCWOkclHw', {
-    filter: 'audioonly',
-  })
-  .on('end', () => {
-    console.log('ytdl finished fetching file');
-  });
-  
-  const ffmpegCommand = ffmpeg()
-  .setFfmpegPath(ejec)
-  .format('mp3')
-  .audioCodec('libmp3lame')
-  .output(ffmpegPipe);
-  
-  video.pipe(passStream);
-  ffmpegCommand.input(passStream).run();
-});/// fin 
-  
+  ce('******************************************************');
+  ce('*******------start downloadFFmpegRender 1-----********');
+  ce('******************************************************'); 
+  var UrlVideoConAudio = req.query.UrlVideoConAudio.replace(/@i/g , "&").replace(/@al/g, "=");
+  var calidad =  req.query.calidad
+  var content = req.query.content;
+  var extencion = req.query.extencion;
+  var urlAudio = req.query.urlAudio.replace(/@i/g , "&").replace(/@al/g, "=");
+  var url = req.query.urlCodificada.replace(/@i/g , "&").replace(/@al/g, "=");
+  var tituloiPlano = req.query.titulo.replace(/[$.,:"'!><?`#~]/g,'');
+ //var videoStream = new stream.PassThrough();
+ //var videoWriteStream = fs.createWriteStream(tituloiPlano+'.mp4');   
+cr(url)
+    function getHttpAudio(){
+        https.get(urlAudio, (data)=>{
+          if (data.statusCode === 200){
+          data.pipe(ffmpegProcess.stdio[5]);
+        } else if (data.statusCode === 302) {
+          getHttpAudio();
+          ce('reiniciando getHttpAudio '+data.statusCode);
+         } else if (data.statusCode === 503) {
+         cr(data.statusCode);
+         res.send('hello world').end()
+         return
+        }
+        data.on('end', () => {
+         ce('******************************************************');
+         ce('*********-      --- end getHttpAudio-       -*********');
+         ce('******************************************************');
+       });
+           
+        }).on("error", (err) => {
+              console.log("Error: " + err.message);
+        });
+        } //end getHttpAudio
+         
+    function getHttpVideo(){
+      https.get(url, (data)=>{
+      if (data.statusCode === 200){
+        data.pipe(ffmpegProcess.stdio[4]);
+      } else if (data.statusCode === 302) {
+        getHttpVideo();
+        ce('reiniciando getHttpAudio '+data.statusCode);
+         } else if (data.statusCode === 503) {
+         cr(data.statusCode);
+/*          res.cookie('title', 'GeeksforGeeks');
+    res.send("Cookie Set"); */
+   
+         return  res.end()
+        } else{
+          return  cr(data.statusCode);
+        }
+      data.on('end', () => {
+       ce('******************************************************');
+       ce('*********-    --- end getHttpVideo-         -*********');
+       ce('******************************************************');
+       });
+         
+      }).on("error", (err) => {
+            console.log("Error: " + err.message);
+      });
+    }
+    getHttpAudio();
+    getHttpVideo();
+// Start the ffmpeg child process
+ const ffmpegProcess = cp.spawn(ejec, [
+  // Remove ffmpeg's console spamming
+  //'-loglevel', '8', 
+  '-hide_banner',
+  // Redirect/Enable progress messages
+  '-progress', 'pipe:3',
+  // Set inputs
+  '-i', 'pipe:4',
+  '-i', 'pipe:5',
+  '-map', '0:v',
+  '-map', '1:a',
+  '-y',
+  // Map audio & video from streams
 
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 
-}
-/////////////////////////////////////////////////////////////////
-app.post('/ffmpeg', cors(), function(req, res) {
-cb('iniciada el ffmpge test')
-var FileScript = path.join(__dirname, 'script', 'FFmpegRender'+moment().format("HH:mm")+'.js');
-var crearFileScript = fs.createWriteStream(FileScript)
-//res.send('ok');
-
-//res.send('ok2');
-///////////////////// descargar script desde> https://uneteamigo.com/js/FFmpegRender.js /////////////////////////////////////////////
-https.get('https://uneteamigo.com/js/FFmpegRender.js', async (dataUrlVid) => {
-  cb('statusCode:', dataUrlVid.statusCode);
-   dataUrlVid.pipe(crearFileScript)
-  .on('error', function(err) {
-   cr('no se pudo guardar el script por el error '+err)
-    return
-  });
-
-  crearFileScript.on("finish", () => {
-    crearFileScript.close();
-    cr("descargado y creado el FFmpegRender.js ejeecutando FFmpgRender ");
-    const FFmpegRender = require(FileScript);
-    FFmpegRender.descargarYcombertirReq(req).then((resp)=>{
-     cr(`enviando res.send(${resp})`);
-     res.send(resp).end();
-   }).catch((error)=>{
-     cr(`enviando error res.send(${error})`);
-     res.send(error).end();
-   });
-  });
+  // Keep encoding
+ // '-c', 'copy',
+ '-c:v', 'copy',
+ '-c:a', 'copy', 
+ //'-b:a', '192k',
+  '-f', 'nut', 'pipe:6',
+  
  
-dataUrlVid.on('data', (d) => {
-  });
-}).on('error', (e) => {
-  console.error('ubo un error al hacer get a la url del script'+e);
-});
-})// fin app.get('/ffmpeg')
+  // Define output file
+
+], {
+   stdio: [
+   
+    'inherit', 'inherit', 'inherit',
+  
+    'pipe', 'pipe', 'pipe', 'pipe',
+  ],
+}); 
+
+ffmpegProcess.stdio[3].on('data', chunk => {
+ 
+ });
+
+ffmpegProcess.on('start', () => {
+ce('inicio')
+})
+if (ffmpegProcess.stdio[6]){
+//ffmpegProcess.stdio[3].pipe(videoStream);
+res.set('Content-disposition', 'attachment; filename='+ encodeURI(tituloiPlano+calidad+"_m_r_b."+extencion));
+res.set('Content-Type', content);
+cr("descargando "+tituloiPlano+calidad+"_m_r_b."+extencion); 
+
+ffmpegProcess.stdio[6].pipe(res)
+}
+
+    })
+//////////////////////////////////////////////////////////////
 
 app.listen(8080, function(){
 console.log('server listo '+moment().format("HH:mm"))
